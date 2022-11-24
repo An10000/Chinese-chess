e369b238d65d84dd241ab693d851df7409adc8eb
hanrenxi <tigershan357@gmail.com>
2022-11-24 18:27:42 -0500

    Andy's commit 4 Implemented ChessGame.ChessGame so that we can start the game with mode selection. Simplified Main.java so that it is easier to debug.

--- csc207project code/View/Main.java
+++ csc207project code/View/Main.java
@@ -1,25 +1,9 @@
 package View;
 
-import Model.Board;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-	Board board;
-	Viewer viewer;
 
 	/**
 	 * Main method
@@ -33,69 +17,7 @@ public class Main extends Application {
 	}
 	@Override
 	public void start(Stage stage) throws Exception {
-		this.board = Board.getInstance();
-
-		stage.setTitle("Chinese Chess");
-
-		BorderPane borderPane = new BorderPane();
-		borderPane.setStyle("-fx-background-color: #121212;");
-
-		//add canvas
-		Canvas canvas = new Canvas(200, 300);
-		canvas.setId("Canvas");
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-
-		final ToggleGroup toggleGroup = new ToggleGroup();
-
-		//add buttons
-		Button classicMode = new Button("Classic Mode");
-		classicMode.setId("Classic Mode");
-		classicMode.setPrefSize(300, 50);
-		classicMode.setFont(new Font(20));
-		classicMode.setStyle("-fx-background-color: #17871b; -fx-text-fill: white;");
-
-		Button timedMode = new Button("Timed Mode");
-		timedMode.setId("Timed Mode");
-		timedMode.setPrefSize(300, 50);
-		timedMode.setFont(new Font(20));
-		timedMode.setStyle("-fx-background-color: #17871b; -fx-text-fill: white;");
-
-		Button scoredMode = new Button("Scored Mode");
-		scoredMode.setId("Scored Mode");
-		scoredMode.setPrefSize(300, 50);
-		scoredMode.setFont(new Font(20));
-		scoredMode.setStyle("-fx-background-color: #17871b; -fx-text-fill: white;");
-
-		VBox controls = new VBox(20, classicMode, timedMode, scoredMode);
-		controls.setPadding(new Insets(20, 20, 20, 20));
-		controls.setAlignment(Pos.CENTER);
-
-		classicMode.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent actionEvent) {
-				Viewer viewer1 = new Viewer("Classic Mode", board, stage);
-			}
-		});
-
-		scoredMode.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent actionEvent) {
-				Viewer viewer1 = new Viewer("Scored Mode", board, stage);
-			}
-		});
-
-		timedMode.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent actionEvent) {
-				Viewer viewer1 = new Viewer("Timed Mode", board, stage);
-			}
-		});
-		borderPane.setTop(controls);
-		borderPane.setCenter(canvas);
-
-		var scene = new Scene(borderPane, 300, 300);
-		stage.setScene(scene);
-		stage.show();
+		ChessGame chessGame = new ChessGame(stage);
 	}
 
 }
