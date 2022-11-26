9d2f6885110aa8952220ed78733af61069725c3e
hanrenxi <tigershan357@gmail.com>
2022-11-26 16:48:44 -0500

    Andy's commit 7 1. Renamed the package to csc207project_code, so we can use it inside. 2. Moved Main.java to csc207project_code package, so it is easier for user to find it. 3. Added a package called csc207project_test, so we can have a test file for each class separately. 4. Moved test in GameTest.java to the csc207project_test, in TestInitializeBoard.java and TestChess.java.

--- csc207project_test/Model/TestChess.java
+++ csc207project_test/Model/TestChess.java
@@ -1,22 +1,16 @@
-import Chesses.*;
-import Control.*;
-import Model.*;
+package Model;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import Chesses.Advisor;
 
-public class GameTest {
+
+public class TestChess {
     @Test
     void testGetLocation() {
         Location expectedLocation = new Location(9, 3);
         Advisor advisor1 = new Advisor(expectedLocation, "Red", "Advisor");
         assertEquals(expectedLocation, advisor1.getLocation());
     }
-    @Test
-    void testInitializeBoard(){
-        InitializeBoard initializeBoard = new InitializeBoard();
-        System.out.println(initializeBoard.getBoard());
-    }
-
-
 }
---
+++ csc207project_test/TestInitializeBoard.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestInitializeBoard {
+    @Test
+    void testInitializeBoard(){
+        InitializeBoard initializeBoard = new InitializeBoard();
+        System.out.println(initializeBoard.getBoard());
+    }
+    @Test
+    void testGetBoard(){
+        InitializeBoard initializeBoard = new InitializeBoard();
+        System.out.println(initializeBoard.getBoard());
+    }
+}
