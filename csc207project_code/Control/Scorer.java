7f19f0965211c1d8c65ba85872e338d5ac7d7b27
hanrenxi <tigershan357@gmail.com>
2022-11-27 14:10:28 -0500

    Andy's commit 8 1. Implemented Scorer.java, including all the methods inside. Added JavaDoc to it. 2. Implemented TestScorer.java for testing.

--- csc207project_code/Control/Scorer.java
+++ csc207project_code/Control/Scorer.java
@@ -1,13 +1,18 @@
 package Control;
 
 import Model.*;
-import View.Viewer;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Scorer {
 
-	private HashMap<String, Integer> scoreBoard;
+	private HashMap<String, Integer> scoreBoard; // scoreBoard that stores score for both factions.
+
+	/**
+	 * Initializer of Scorer. Called at the beginning of Scored Mode game.
+	 * Initializes the scoreBoard, and put the score for "Red" and "Black" faction 0.
+	 */
 	public Scorer(){
 		this.scoreBoard = new HashMap<>();
 		scoreBoard.put("Red", 0);
@@ -15,22 +20,41 @@ public class Scorer {
 	}
 
 	/**
-	 * 
-	 * @param faction
+	 * return the score of current faction.
+	 * if faction == Red then return score of the Red faction
+	 * if faction == Black then return score of the Black faction
+	 * @param faction The faction of interest, either "Red" or "Black", in String format.
 	 */
 	public int getFactionScore(String faction) {
-		// TODO - implement Scorer.getFactionScore
-		throw new UnsupportedOperationException();
+		return scoreBoard.get(faction);
+//		throw new UnsupportedOperationException();
+
 	}
 
 	/**
-	 * 
-	 * @param faction
-	 * @param killed
+	 * add score to faction when a chess of the opposite faction is killed.
+	 *
+	 * @param killed the chess being killed.
 	 */
-	public void addScore(String faction, Chess killed) {
-		// TODO - implement Scorer.addScore
-		throw new UnsupportedOperationException();
+	public void addScore(Chess killed) {
+		if (Objects.equals(killed.getFaction(), "Black")){//if the chess is Black
+			int originalScore = scoreBoard.get("Red");
+			if (!Objects.equals(killed.getType(), "General")){
+				scoreBoard.put("Red", originalScore + 1);//add 1 to Red
+			} else{
+				scoreBoard.put("Red", originalScore + 10);//add 10 to Red
+			}
+		} else if (Objects.equals(killed.getFaction(), "Red")) {//if the chess is Red
+			int originalScore = scoreBoard.get("Black");
+			if (!Objects.equals(killed.getType(), "General")){
+				scoreBoard.put("Black", originalScore + 1);//add 1 to Black
+			} else{
+				scoreBoard.put("Black", originalScore + 10);//add 10 to Black
+			}
+		} else{
+			throw new RuntimeException("faction is not Black nor Red!");// we cannot recognize the faction!
+		}
+		// throw new UnsupportedOperationException();
 	}
 
 }
