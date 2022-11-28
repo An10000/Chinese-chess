be6438213cb7baec0dff7414bc9493732801f22c
hanrenxi <tigershan357@gmail.com>
2022-11-27 20:40:43 -0500

    Andy's commit 9 1. Implemented Controller.java, including all the methods inside. Tests are not written because I can't initialize stage from javafx. 2. Added javaDoc for NullChess.java and implemented NullChess.getNextPosition().

--- csc207project_code/Chesses/NullChess.java
+++ csc207project_code/Chesses/NullChess.java
@@ -1,23 +1,38 @@
 package Chesses;
 import Model.*;
 
+import java.util.ArrayList;
+
 public class NullChess extends Chess{
 
 
+    /**
+     * Constructor of NullChess. it is just inherited from Chess.
+     * @param location the location the chess is on
+     * @param faction the faction the chess is
+     * @param type the type of the chess, for NullChess it is Null
+     */
     public NullChess(Location location, String faction, String type) {
-        // TODO - implement Chess.Chess
-//		throw new UnsupportedOperationException();
         super(location, faction, type);
     }
 
     /**
-     *
-     * @param destination
-     * @param board
+     * null chess cannot move!
+     * api is still here.
+     * @param destination the location the chess move to
+     * @param board the board the game is on
      */
     public void move(Location destination, Board board) {
-        // TODO - implement General.move
         throw new UnsupportedOperationException();
+    }
+
+    /**
+     * null chess has no next positions
+     * @param board the board the game is on
+     * @return a new arraylist that doesn't contain an element.
+     */
+    public ArrayList<Location> getNextPosition(Board board){
+        return new ArrayList<>();
     }
 
 }
--- csc207project_code/Control/Controller.java
+++ csc207project_code/Control/Controller.java
@@ -1,34 +1,126 @@
 package Control;
 
+import Chesses.NullChess;
 import Model.*;
 import View.*;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Controller {
 
+	private Board board;
 	private Player[] players;
 	private String mode;
+	private Viewer viewer;
+	private Scorer scorer;
 
 	/**
-	 * 
-	 * @param chess
-	 * @param destination
+	 *
 	 */
-	public boolean moveRequest(Chess chess, Location destination) {
-		// TODO - implement Controller.moveRequest
-		throw new UnsupportedOperationException();
+	private ArrayList<Location> clicks;
+
+	public static final int OK = 0;
+	public static final int MOVED = 1;
+	public static final int INVALID = 2;
+	/**
+	 * request a move of a chess.
+	 * if the chess can move to the destination,
+	 * then move and kill the chess on that location(even null chess) return the chess being killed
+	 * if not then return null.
+	 * @param chess the chess of interest
+	 * @param destination the target location
+	 */
+	public Chess moveRequest(Chess chess, Location destination) {
+		ArrayList<Location> nextLoctions = chess.getNextPosition(board);
+		if (nextLoctions.contains(destination)){
+			Chess killedChess = board.getChessAt(destination);
+			if (killedChess.getType() == null){
+				chess.move(destination, board);
+				return killedChess;
+			}
+			else{
+				chess.kill(killedChess);
+				return killedChess;
+			}
+
+		}else{
+			return null;
+		}
+//		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * 
-	 * @param board
-	 * @param players
-	 * @param mode
-	 * @param viewer
-	 * @param scorer
+	 * handle a new click from the user.
+	 * A user have to first click on a chess on his own faction, then click on a place the chess can go.
+	 * if the click is on his own chess, then it is OK;
+	 * if the click is on an empty space or an enemy chess, then
+	 *     if he has already clicked on one of his own chesses, try if the movement is possible by calling moveRequest
+	 *     if not, it is INVALID
+	 * if the click is not on any chess, then INVALID
+	 * @param location The location the user clicked
+	 * @param faction The faction the user is currently at
+	 * @return the type of the click, by int constant
+	 */
+	public int addClick(Location location, String faction){
+		if (board.getChessList()[location.getRow()][location.getCol()].getFaction() == faction){
+			clicks.clear();
+			clicks.add(location);
+			return OK;
+		}
+		else if (board.getChessList()[location.getRow()][location.getCol()].getFaction() == null || !Objects.equals(board.getChessList()[location.getRow()][location.getCol()].getFaction(), faction)){
+			if (clicks.size() <= 1){//if not clicked previously
+				clicks.clear();
+				return INVALID;
+			}
+			else{
+				Chess killed = moveRequest(board.getChessList()[clicks.get(1).getRow()][clicks.get(1).getCol()], clicks.get(0));
+				if (mode == "Scored Mode" && killed != null){
+					if (killed.getType() != "NullChess"){
+						scorer.addScore(killed);
+					}
+				}
+				else if (killed == null){
+					return INVALID;
+				}
+				clicks.clear();
+				return MOVED;
+			}
+		}
+		else{
+			clicks.clear();
+			return INVALID;
+		}
+	}
+
+	/**
+	 * The initializer of controller,
+	 * @param board the board the players are playing
+	 * @param players the players of the game
+	 * @param mode mode of the game
+	 * @param viewer add viewer to the controller
+	 * @param scorer add scorer to the controller
 	 */
 	public Controller(Board board, Player[] players, String mode, Viewer viewer, Scorer scorer) {
-		// TODO - implement Controller.Controller
-		throw new UnsupportedOperationException();
+		this.board = board;
+		this.players = players;
+		this.mode = mode;
+		this.viewer = viewer;
+		if (scorer != null){
+			this.scorer = scorer;
+		}
+		else if (Objects.equals(mode, "Scored Mode")){
+			throw new RuntimeException("scorer is not defined! ");
+		}
+		clicks = new ArrayList<>();
+	}
+
+	/**
+	 * getter method for clicks
+	 * @return clicks stored
+	 */
+	public ArrayList<Location> getClicks() {
+		return clicks;
 	}
 
 }
