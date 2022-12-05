cdc983f349455b65c33679be9d454ed7e5c1ac44
RichieHsieh <richie.hsieh@mail.utoronto.ca>
2022-12-05 00:21:07 -0500

    Commit 14 of CSC207 project by Richie. Finish the game ending and checkmate visualization. Update View.Timer, solved the issue that the timer updating will be delay while change round.

--- csc207project_code/View/Timer.java
+++ csc207project_code/View/Timer.java
@@ -4,7 +4,10 @@ import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -15,43 +18,62 @@ public class Timer {
 	private Stage stage;
 	private String curr_round;
 	private int counter;
+	private int count;
 
+	/**
+	 * The constructor of Timer.
+	 * @param time  An integer that represents the duration of each round in second.
+	 * @param viewer  The viewer that is used to visualize the game.
+	 * @param stage  The stage that is used to build visualization.
+	 */
 	public Timer(int time, Viewer viewer, Stage stage) {
 		this.roundTime = time;
 		this.viewer = viewer;
 		this.curr_round = viewer.getRound();
 		this.counter = time;
 		this.stage = stage;
+		this.count = 0;
 		countDown();
 	}
 
+	/**
+	 * The method will create a new timeline to countdown.
+	 */
 	private void countDown() {
-		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-		final Runnable runnable = new Runnable() {
-			int countdownStarter = roundTime;
-			public void run() {
-
-				counter = countdownStarter;
-				countdownStarter--;
-				if (!Objects.equals(curr_round, viewer.getRound())) {
-					countdownStarter = roundTime;
-					curr_round = viewer.getRound();
-				}
-
-				if (countdownStarter == -1) {
-					countdownStarter = roundTime;
-					viewer.nextRound();
-					curr_round = viewer.getRound();
-				}
-
-				stage.setOnCloseRequest(event -> {
-					scheduler.shutdown();
-				});
-			}
-		};
-		scheduler.scheduleAtFixedRate(runnable, 0, 1, SECONDS);
+		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.25), e -> updateTime()));
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.play();
 	}
 
+	/**
+	 * The method will update the current time that the player has,
+	 * and notify the viewer about the change of time.
+	 */
+	private void updateTime() {
+		if (!viewer.checkEnd()) {
+			count++;
+			System.out.println(count);
+			if (count == 4) {
+				System.out.println("run!");
+				counter--;
+				count = 0;
+				System.out.println(counter);
+			}
+			if (counter == 0) {
+				viewer.nextRound();
+				counter = roundTime;
+			}
+			else if (!curr_round.equals(viewer.getRound())) {
+				counter = roundTime;
+				curr_round = viewer.getRound();
+			}
+		}
+	}
+
+	/**
+	 * Return the remaining time of current round in second.
+	 * @return counter
+	 */
 	public int getCounter() {
 		return counter;
 	}
